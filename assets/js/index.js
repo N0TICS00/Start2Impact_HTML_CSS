4b124a4893a2c38bde90b2654f8d89c7f50d497d
N0TICS0 <74853755+N0TICS0@users.noreply.github.com>
2021-08-14 12:07:43 +0200

    Commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,17 +2,17 @@
 
 
 //TEXT ANIMATION
-let words = ["Programmer" , "Baseballer" , "Reader" , "Digital Passionate"]
+let words = ["Programmer" , "Baseballer" , "Reader" , "Digital Passionate" , "Video Gamer"]
 // Check the path
 if(window.location.pathname === "/assets/italian-version/index.html"){
-    words = ["Programmatore" , "Baseballer" , "Lettore" , "Appassionato Digitale"]
+    words = ["Programmatore" , "Baseballer" , "Lettore" , "Appassionato Digitale" , "Video Gamer"]
 }
 
 let counter = 0
 let word = document.querySelector(".animation-text")
 setInterval(textAnimation, 1000)
 function textAnimation(){
-    word.innerHTML = words[counter]
+    word.innerHTML = words[counter] //Change the Text 
     counter++
     if(counter >= words.length){
         counter=0
